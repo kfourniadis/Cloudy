22f34ab22346a5db7f3add9f07342c90b0c42105
Martin Mlostek <mlostek@gmail.com>
2020-12-07 20:11:12 +0100

    added microphone permission request

--- Cloudy/App/AppDelegate.swift
+++ Cloudy/App/AppDelegate.swift
@@ -2,6 +2,7 @@
 
 import UIKit
 import Firebase
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Initialize firebase
         FirebaseApp.configure()
+        // request mic permissions
+        AVAudioSession.sharedInstance().requestRecordPermission { _ in }
         // Override point for customization after application launch.
         return true
     }
